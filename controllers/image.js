9ea35eb81bcb3cb11f879e12af9b1d62f7657f34
mefa <mfbyrktr@gmail.com>
2020-05-02 00:48:27 +0300

    gitignore added

--- controllers/image.js
+++ controllers/image.js
@@ -4,8 +4,9 @@ const app = new Clarifai.App({apiKey: '77b1488d057a43e09bb11a45ef9724f4'});
 const handleApiCall = (req,res) => {
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-      .then(data => res.json(data))
-      .catch(err => { res.status(404).json("cannot handleApiCall")})
+      .then(data => console.log)
+      /* .then(data => res.json(data)) */
+      /* .catch(err => { res.status(404).json("cannot handleApiCall")}) */
 }
 
 
